d514ecd9570c641cf35b621c0cfdc58693980f93
beckian <beckian@umich.edu>
2023-05-11 12:46:23 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+import discord
+from dotenv import load_dotenv
+
+
+load_dotenv()
+token = os.getenv("token")
+client = discord.Client()
+client.run(str(token))
+    
+@client.event
+async def on_ready():
+    print(f"{client.user} has connected to Discord!")
