427d6ebfff23cd00717c08eeffb8a4ad3f035326
beckian <beckian@umich.edu>
2023-05-11 14:27:20 -0400

    move

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ from dotenv import load_dotenv
 load_dotenv()
 token = os.getenv("token")
 client = discord.Client(intents=discord.Intents.all())
-client.run(token)
     
 @client.event
 async def on_ready():
     print(f"{client.user} has connected to Discord!")
+
+client.run(token)
