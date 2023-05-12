b15f959c9cc5c55af5838b57f9c1092056de74a9
beckian <beckian@umich.edu>
2023-05-12 12:52:52 -0400

    asdf

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ async def on_message(msg):
         await msg.channel.send('where the bitches at')
         
     print(msg.channel)
-    if msg.channel == 'bot-test':
+    if msg.channel == 'bot test':
         print('hi')
+
 client.run(token)
