6957793b89e74b23f28d91843d9778da7d949be0
beckian <beckian@umich.edu>
2023-05-31 16:15:36 -0400

    rat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import random
 import discord
 from dotenv import load_dotenv
 
@@ -43,9 +44,16 @@ async def on_message(msg):
             for role in msg.author.roles:
                 if role.name == 'gay boy':
                     taz = True
-            print(msg.author.roles)
+                    break
             if not taz:
                 await msg.channel.send('"Real Emo" only consists of the dc Emotional Hardcore scene and the late 90\'s Screamo scene. What is known by "Midwest Emo" is nothing but Alternative Rock with questionable real emo influence. When people try to argue that bands like My Chemical Romance are not real emo, while saying that Sunny Day Real Estate is, I can\'t help not to cringe because they are just as fake emo as My Chemical Romance (plus the pretentiousness). Real emo sounds ENERGETIC, POWERFUL and somewhat HATEFUL. Fake emo is weak, self pity and a failed attempt to direct energy and emotion into music. Some examples of REAL EMO are Pg 99, Rites of Spring, Cap n Jazz (the only real emo band from the midwest scene) and Loma Prieta. Some examples of FAKE EMO are American Football, My Chemical Romance and Mineral EMO BELONGS TO HARDCORE NOT TO INDIE, POP PUNK, ALT ROCK OR ANY OTHER MAINSTREAM GENRE')
+
+    if 'rat' in msg.content.lower():
+        pic = random.choice(
+            'https://www.peta.org/wp-content/uploads/2015/04/10903825_872344489483233_7702124773103899276_o-668x336.jpg?20190103121630',
+            
+        )
+        
 
 @client.event
 async def on_raw_reaction_add(payload):
