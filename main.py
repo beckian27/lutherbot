3ae95f4f28195d3d026b00b6736e9bb17e458513
beckian <beckian@umich.edu>
2023-06-06 12:04:48 -0400

    comments

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ async def on_message(msg):
     
     if 'emo' in msg.content.lower(): 
         if not msg.author.bot and msg.channel.name != 'makeup-chores':
-            if msg.author.name != 'taztaztaz':
+            if msg.author.name != 'taztaztaz': # he got annoyed
                 await msg.channel.send('"Real Emo" only consists of the dc Emotional Hardcore scene and the late 90\'s Screamo scene. What is known by "Midwest Emo" is nothing but Alternative Rock with questionable real emo influence. When people try to argue that bands like My Chemical Romance are not real emo, while saying that Sunny Day Real Estate is, I can\'t help not to cringe because they are just as fake emo as My Chemical Romance (plus the pretentiousness). Real emo sounds ENERGETIC, POWERFUL and somewhat HATEFUL. Fake emo is weak, self pity and a failed attempt to direct energy and emotion into music. Some examples of REAL EMO are Pg 99, Rites of Spring, Cap n Jazz (the only real emo band from the midwest scene) and Loma Prieta. Some examples of FAKE EMO are American Football, My Chemical Romance and Mineral EMO BELONGS TO HARDCORE NOT TO INDIE, POP PUNK, ALT ROCK OR ANY OTHER MAINSTREAM GENRE')
 
     if 'rat' in msg.content.lower() and not msg.author.bot:
@@ -107,7 +107,6 @@ async def on_raw_reaction_add(payload):
     if payload.channel_id == MAKEUP_ID:
         server = await client.fetch_guild(SERVER_ID)
         user = await server.fetch_member(payload.user_id)
-        print(user.name)
         
         if not user.bot:
             channel = client.get_channel(MAKEUP_ID)
