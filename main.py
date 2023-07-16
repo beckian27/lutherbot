15d3aa0c959b646f553cb5bdee5a1b78c43352ce
root <root@DESKTOP-QN03J9G.localdomain>
2023-07-16 16:21:28 -0400

    factoring

--- main.py
+++ main.py
@@ -56,21 +56,7 @@ async def on_raw_reaction_add(payload):
                 
     # allows claiming of makeup chore opportunities or deletion by worm
     if payload.channel_id == MAKEUP_ID:
-        server = await client.fetch_guild(SERVER_ID)
-        user = await server.fetch_member(payload.user_id)
-        
-        if not user.bot:
-            channel = client.get_channel(MAKEUP_ID)
-            message = await channel.fetch_message(payload.message_id)
-            
-            if message.author.bot:
-                text = message.content
-                await channel.delete_messages([message])
-                
-                if not user.get_role(WORM):
-                    text = text.removesuffix('\nClick the check mark to claim this chore!')
-                    text = f'{text}\n{user.display_name} claimed this chore!'
-                    await channel.send(text)
+        await makeups.claim_makeup(payload, client, SERVER_ID, MAKEUP_ID, WORM)
 
 client.run(token)
 
--- makeups.py
+++ makeups.py
@@ -5,3 +5,20 @@ async def create_makeup(msg, check):
         await msg.channel.delete_messages([msg])
         msg = await msg.channel.send(opportunity)
         await msg.add_reaction(check)
+
+async def claim_makeup(payload, client, serve, makeup, worm):
+    server = await client.fetch_guild(serve)
+    user = await server.fetch_member(payload.user_id)
+    
+    if not user.bot:
+        channel = client.get_channel(makeup)
+        message = await channel.fetch_message(payload.message_id)
+        
+        if message.author.bot:
+            text = message.content
+            await channel.delete_messages([message])
+            
+            if not user.get_role(worm):
+                text = text.removesuffix('\nClick the check mark to claim this chore!')
+                text = f'{text}\n{user.display_name} claimed this chore!'
+                await channel.send(text)
