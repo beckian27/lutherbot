19acf8ff8857dd3587264704095e5063e690d36d
LutherWorm <luther.workmanager@icc.coop>
2025-05-18 19:25:47 -0400

    Update config.py

--- config.py
+++ config.py
@@ -33,5 +33,5 @@ USERNAMES = {
     'bleachblondeglinda': 'Serafina Sabatini',
     'bendy_enby': 'Jasper VanHorne',
     'damiandarkbrighte': 'Amina' ,
-    'batgrrrl1291' : 'Marie'
+    'batgrrrl1291' : 'Marie Kazmierski'
 }
