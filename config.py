b58c727b925b0b086aa1d7d8177acd67a37e4799
LutherWorm <luther.workmanager@icc.coop>
2026-09-08 18:26:43 -0400

    Update config.py

--- config.py
+++ config.py
@@ -5,10 +5,6 @@ SCHEDULE = 'Summer 2026 Chore Schedule'
 
 # key for matching discord names to names in the spreadsheet, needs to be manually updated
 USERNAMES = {
-    'lights0123': 'Ben Schattinger',
-    'dathrax.': 'Jack Handzel',
-    'madisonisdead': 'Madison Dennis',
-    'avameester': 'Ava Meester',
     'c.harles2408': 'Charles Coles',
     'delia9860': 'Delia Harris',
     'jeephater3000': 'Eliza Hammond',
@@ -17,39 +13,32 @@ USERNAMES = {
     'jannes7297': 'Jannes Tiemes', 
     'jasonl': 'Jason Debolski',
     'futurev1ctorian': 'Judah Nicolasa',
-    'sudani.mo': 'Mohamed Hashim',
     'hauntedskull': 'Toby Mayo',
     '_tommy_9949': 'Tommy Taylor',
     '.superspider': 'Vincent Ferrara',
-    'sydney027921': 'Sydney DeWaide',
-    'tripforte': 'Shane Collins',
-    'lesbiancomrademiku': 'Rocke Ramsey',
-    'abug22': 'Aaron Bugner',
-    'fxjupiter': 'Charlie Swan',
-    '.deathbyhamster': 'Jagger Pacheco',
-    'yopinky': 'Olivia Korensky',
-    'gr0ss': 'Alexandra Alanis',
-    'ethanjohnson24': 'Ethan Johnson',
-    'isopodbowls': 'Eve Sotham',
     'woba6y4748': 'Layla Salaheldin',
     'skalvert': 'Sasha Kalvert',
-    'will019319': 'William McCall',
-    'ioana.jpeg': 'Ioana Dumitrascu',
-    'christjesus7971': 'Christian Loza',
-    'bendy_enby': 'Jasper VanHorne',
-    'vivcifi' : 'Vivian Branham',
-    'nyasper_0677': 'Zee',
     'wasomijelili': 'Eli Robinson',
-    'madi_.13': 'Madison Leads', 
     'mk3makayla': 'Makayla Apulello', 
-    'divockorigi0647': 'Caleb Shoup',
     'jasonl': 'Jason',
     'gobbybear': 'Jamie',
-    'molly037353': 'Molly',
-    'rofenil': 'Danny',
-    'randomgaming_': 'Jacob Dvorin',
     'smalltestacount': 'Alexa'
-    
+    'beeremic' : 'Michael Beerens'
+    'xiaoyuchen_55835' : 'Xiaoyu Chen'
+    'potatoterrier' : 'Sophia Homyak'
+    'bird.c.three' : 'Jay Hatch'
+    'loser.inc' : 'Abigal Adams'
+    'wrenwolfinator' : 'Spencer Caldwell'
+    'ianp0524_40358' : 'Ian Patterson'
+    'gobbybear' : 'James Spencer'
+    'kreuzificado' : 'Grayson Gallardo'
+    'mikviper' : 'Julian Lorenz'
+    'aliyapea' : 'Aliya Shirato-White'
+    'mygoodbuddyjuno' : 'Juno Krishti'
+    'onesnoffledshotgun' : 'Leo Turyk'
+    'mugglebee' : 'Jack Mugglebee' 
+    'well_thats_not_good' : 'Malcolm Keaton'
+    'zanejulian' : 'Luca Rowe'
     
     
     
